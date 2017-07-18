7420f3d2ddd47c2dc134edc7fc7d2292d143fcf4
Tevin Vu <tevinvu@gmail.com>
2017-07-17 18:34:29 -0700

    Chapter 02 ex 02

---
+++ chapter02/ch02ex02.c
@@ -0,0 +1,15 @@
+#include <stdio.h>                      //directives
+
+int main(void)
+{
+    printf("Parkinson's Law:\nWork expands so as to ");         //statement
+    printf("fill the time\n");                                  //statement
+    printf("available for its completion.\n");                  //statement
+    return 0;                                                   //statement
+}
+
+
+//run: make ch01ex02
+//./ch01ex02
+
+

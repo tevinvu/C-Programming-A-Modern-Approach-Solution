656acb264522b481fdc46d566565e342aa86b97e
Tevin Vu <tevinvu@gmail.com>
2017-07-17 19:23:59 -0700

    Chapter 02 project 01

---
+++ chapter02/ch02proj01.c
@@ -0,0 +1,18 @@
+#include <stdio.h>
+
+int main(void)
+{
+    printf("       *\n");
+    printf("      *\n");
+    printf("     *\n");
+    printf("*   *\n");
+    printf(" * *\n");
+    printf("  *\n");
+    printf("\n");
+    return 0;
+}
+
+
+//run:
+//make ch01proj01
+//./ch01proj01

857a99b454234081cdfaf29b33bace72b3dc22b3
Tevin Vu <tevinvu@gmail.com>
2017-07-17 17:52:12 -0700

    Chapter 02 ex 01

---
+++ chapter02/ch02ex01.c
@@ -0,0 +1,13 @@
+#include <stdio.h>
+
+int main(void)
+{
+    printf("hello, world\n");
+}
+
+//I don't get a warning message from the compiler
+//here how i run it:
+//make ch01ex01
+//./ch01ex01
+//print out screen: hello, world
+

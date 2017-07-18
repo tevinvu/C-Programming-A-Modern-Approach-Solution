7ec5b2fea49ff615387272a2ca763664a735b768
Tevin Vu <tevinvu@gmail.com>
2017-07-18 15:54:02 -0700

    Chapter 02 project 02

---
+++ chapter02/ch02proj02.c
@@ -0,0 +1,15 @@
+#include <stdio.h>
+
+int main(void)
+{
+    int r = 10;
+    float pi = 3.14;
+    printf("Volume of a sphere with a 10-meter radius: %.2f", 4.0f/3.0f * pi * r * r * r);
+    printf("\n");
+    return 0;
+}
+
+
+//run:
+//make ch01proj02
+//./ch01proj02

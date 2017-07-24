1740bdb2ddedc649e5703fb2b87fba23993fee6d
Tevin Vu <tevinvu@gmail.com>
2017-07-24 10:57:10 -0700

    Chapter 03 ex 02

---
+++ chapter03/ch03ex02.c
@@ -0,0 +1,20 @@
+#include <stdio.h>                      //directives
+
+int main(void)
+{
+    float x;
+    printf("%-8.1e", x);
+    printf("%10.6e", x);
+    printf("%-8.3f", x);
+    printf("%6.0f", x);
+    
+   printf("\n");
+   return 0;
+   
+}
+
+
+//run: make ch02ex01
+//     ./ch02ex01
+
+

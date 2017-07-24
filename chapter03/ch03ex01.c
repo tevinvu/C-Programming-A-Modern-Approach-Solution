1dfcfe0c782f08e2b3a30cd215b2cb24411daad2
Tevin Vu <tevinvu@gmail.com>
2017-07-24 10:01:05 -0700

    Chapter 03 ex 01

---
+++ chapter03/ch03ex01.c
@@ -0,0 +1,19 @@
+#include <stdio.h>                      //directives
+
+int main(void)
+{
+    printf(" %6d, %4d", 86, 1040);
+    printf("%12.5e", 30.253);
+    printf("%.4f", 83.162);
+    printf("%-6.2g", .0000009979);
+    
+   printf("\n");
+   return 0;
+   
+}
+
+
+//run: make ch02ex01
+//     ./ch02ex01
+
+

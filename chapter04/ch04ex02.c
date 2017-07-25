26f9c6585d51164dac9f31dbd7b83fc0e1754c88
Tevin Vu <tevinvu@gmail.com>
2017-07-25 10:52:38 -0700

    Chapter 04 exercise 02

---
+++ chapter04/ch04ex02.c
@@ -0,0 +1,16 @@
+#include <stdio.h>
+
+int main(void)
+{
+    printf(" 8/ 5 = %d\n", 8/5);
+    printf(" -8/ 5 = %d\n", -8/5);
+    printf(" 8/ -5 = %d\n", 8/-5);
+    printf(" -8/ -5 = %d\n", -8/-5);
+    printf("\n");
+    return 0;
+}
+
+
+//run:
+//make ch04ex02
+//./ch04ex02
